ac16f6088137e7922fe179a72feb33b7d55887d4
HK <28485479+hbkalariaassociates@users.noreply.github.com>
2023-03-06 12:26:45 +0530

    added styling

--- polls_app/apps.py
+++ polls_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PollsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "polls_app"
